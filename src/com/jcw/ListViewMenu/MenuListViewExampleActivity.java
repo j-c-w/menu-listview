4755f90f7909c76cf88a4e31163f7b40141a96bd
Jackson Woodruff <woodruff.jackson@gmail.com>
2015-04-20 11:17:19 +0100

    added an example of menus multiple levels deep

--- src/com/jcw/ListViewMenu/MenuListViewExampleActivity.java
+++ src/com/jcw/ListViewMenu/MenuListViewExampleActivity.java
@@ -29,7 +29,18 @@ public class MenuListViewExampleActivity extends Activity {
 			}
 		}));
 
+		List<MenuListItem> subSubItems = new ArrayList<MenuListItem>();
+		subSubItems.add(new MenuListItem("This is doubly deep in a menu", new MenuItemClickListener() {
+			@Override
+			public void onClick(String text, String subheader, boolean isFolder) {
+				Toast.makeText(MenuListViewExampleActivity.this, text, Toast.LENGTH_LONG).show();
+			}
+		}));
+
+		items.add(new MenuListFolder("Folder", "You can have sub folders too!", subSubItems));
+
 		MenuListFolder container = new MenuListFolder("Folder", "Click to enlarge", items);
+
 
 		adapter.addItem(container);
 		adapter.addItem(new MenuListItem("Exit", "Quit the demo", new MenuItemClickListener() {
